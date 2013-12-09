496c3942de47a16eb9697c6c038e954da4ce81d4
MinRK <benjaminrk@gmail.com>
2013-12-08 22:24:23 -0800

    test import from package without touching sys.path

--- test.py
+++ test.py
@@ -2,19 +2,27 @@ import sys
 from os import path
 
 
-# Put site-packages in front of sys.path so we don't end up importing the global
-# readline.so
-sys.path = (
-    [p for p in sys.path if 'site-packages' in p] + \
-    [p for p in sys.path if 'site-packages' not in p])
+def test_pkg_import():
+    """import without touching sys.path"""
+    from python_readline import readline
 
 
 def test_import():
     """A very basic unittest; can we 'import readline'?"""
     msg = r'''
-readline.so was not installed properly into site-packages.
-'import readline' imports %s
-sys.path:\n%s'''
+    readline.so was not installed properly into site-packages.
+    'import readline' imports %s
+    sys.path:\n%s'''
+    # Put site-packages in front of sys.path so we don't end up importing the global
+    # readline.so
+    save_sys_path = list(sys.path)
+    sys.path = (
+        [p for p in sys.path if 'site-packages' in p] + \
+        [p for p in sys.path if 'site-packages' not in p])
+    
     import readline
-    assert 'site-packages' in path.dirname(readline.__file__), \
+    try:
+        assert 'site-packages' in path.dirname(readline.__file__), \
             msg % (readline.__file__, '\n'.join(sys.path))
+    finally:
+        sys.path = save_sys_path
