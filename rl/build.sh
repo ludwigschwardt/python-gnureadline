eced5a3e1c0d6b96c74938dc7ca0a575c1c95a8b
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2014-04-04 21:12:19 +0200

    Use $CC or cc to compile libreadline on Darwin
    Force the compiler to CC (or cc by default) to ensure that the
    architecture flag detection is done on the same compiler that is used to
    compile libreadline. Without this libreadline will default to gcc, which
    might not be the same as cc and also does not support -arch if it is the
    real gcc. The Python extension module will typically be compiled by clang
    on Darwin, which should be the default cc.
    
    This follows @ohanar's suggestion in issue #28.

--- rl/build.sh
+++ rl/build.sh
@@ -4,7 +4,15 @@ set -e
 # If we are on Mac OS X, try to do a universal build, as this will allow the
 # Python extension to pick the architectures it needs from the static library
 if [ `uname` == "Darwin" ]; then
-	detected_arches="Detected architectures:"
+	# Force the compiler to CC (or cc by default) to ensure that the architecture
+	# flag detection is done on the same compiler that is used to compile libreadline.
+	# Without this libreadline will default to gcc, which might not be the same as cc
+	# and also does not support -arch if it is the real gcc. The Python extension module
+	# will typically be compiled by clang on Darwin, which should be the default cc.
+	osx_compiler=${CC-cc}
+	cc_override="CC=$osx_compiler"
+	echo "Using compiler $osx_compiler on OS X"
+	detected_arches="Compiler supports architectures:"
 	echo $'#include <stdio.h>\nint main() {\n  printf("Hello world\\n");\n}' > test.c
 	# Find out which architectures are supported by doing a test compile AND link
 	for arch in i386 x86_64 ppc ppc64; do
@@ -29,6 +37,7 @@ patch -p0 < ../readline62-002
 patch -p0 < ../readline62-003
 patch -p0 < ../readline62-004
 patch -p0 < ../readline62-005a
-./configure CPPFLAGS='-DNEED_EXTERN_PC -fPIC'
+# Force compiler to CC/cc in the case of Darwin
+./configure CPPFLAGS='-DNEED_EXTERN_PC -fPIC' $cc_override
 # Only the static libraries are required
 make static
