283bed7273f5c8738e49713aba0fe3481ed9716a
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2015-09-27 00:43:52 +0200

    Fix compilation with GNU gcc
    For some obscure reason the (real) gcc compiler refuses to compile when
    presented with the flags '-arch i386 -arch x86_64', instead of just picking the
    last item as promised. See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60821
    for more details.
    
    This is fixed by detecting the scenario (multiple arches don't work and they
    are i386 + x86_64, in that order) and then reverting to x86_64 only (or nothing
    to trigger the default architecture).
    
    This addresses issue #44.

--- rl/build.sh
+++ rl/build.sh
@@ -14,17 +14,31 @@ if [ `uname` == "Darwin" ]; then
 	echo "Using compiler $osx_compiler on OS X"
 	detected_arches="Compiler supports architectures:"
 	echo $'#include <stdio.h>\nint main() {\n  printf("Hello world\\n");\n}' > test.c
-	# Find out which architectures are supported by doing a test compile AND link
+	# Find out which architectures are supported by doing a test compile AND link per arch
+  arch_flags=''
 	for arch in i386 x86_64 ppc ppc64; do
 		arch_flag=' -arch '$arch
 		if ${CC-cc} $arch_flag test.c 2> /dev/null; then
-			CFLAGS+=$arch_flag
+			arch_flags+=$arch_flag
 			detected_arches+=' '$arch
 		fi
 	done
-	rm -f test.c test.o a.out
 	echo $detected_arches
+  # Verify the final arch flags (typically containing multiple architectures).
+  # If it doesn't work, we probably have GNU gcc which cannot handle
+  # i386 + x86_64 (in that order) - go for x86_64 only instead.
+	if ! ${CC-cc} $arch_flags test.c 2> /dev/null; then
+    if test "$arch_flags" == ' -arch i386 -arch x86_64'; then
+      echo "Compiler (gcc?) fails with both i386 and x86_64 arches - do x86_64 only"
+      arch_flags=' -arch x86_64'
+    else
+      echo "Compiler fails with multiple arches - go back to default architecture"
+      arch_flags=''
+    fi
+	fi
+	rm -f test.c test.o a.out
 	# Don't set sysroot anymore (should be fine for OS X 10.6+)
+  CFLAGS+=$arch_flags
 	export CFLAGS
 fi
 
