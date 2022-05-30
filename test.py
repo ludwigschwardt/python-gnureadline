4c35e3b502e05857999b87ce1e2eac1d908ff42c
Ludwig Schwardt <ludwig@ska.ac.za>
2022-05-30 16:12:08 +0200

    Add a basic functionality unit test
    This just checks that the module is not completely useless. Taken
    from the CPython unit tests in Lib/test/test_readline.py.

--- test.py
+++ test.py
@@ -19,13 +19,39 @@ def test_import():
     sys.path = (
         [p for p in sys.path if 'site-packages' in p] + \
         [p for p in sys.path if 'site-packages' not in p])
-    
+
     import readline
     try:
         assert 'site-packages' in path.dirname(readline.__file__), \
             msg % (readline.__file__, '\n'.join(sys.path))
     finally:
         sys.path = save_sys_path
-    
+
     import gnureadline
     assert readline.parse_and_bind is gnureadline.parse_and_bind
+
+
+def test_history_manipulation():
+    """Basic readline functionality checks taken from Lib/test/test_readline.py."""
+    import gnureadline
+
+    gnureadline.clear_history()
+    gnureadline.add_history("first line")
+    gnureadline.add_history("second line")
+
+    assert gnureadline.get_history_item(0) is None
+    assert gnureadline.get_history_item(1) == "first line"
+    assert gnureadline.get_history_item(2) == "second line"
+
+    gnureadline.replace_history_item(0, "replaced line")
+    assert gnureadline.get_history_item(0) is None
+    assert gnureadline.get_history_item(1) == "replaced line"
+    assert gnureadline.get_history_item(2) == "second line"
+
+    assert gnureadline.get_current_history_length() == 2
+
+    gnureadline.remove_history_item(0)
+    assert gnureadline.get_history_item(0) is None
+    assert gnureadline.get_history_item(1) == "second line"
+
+    assert gnureadline.get_current_history_length() == 1
