60e1d404fd6408bc7aab4ef5de5c3174be5b77bb
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2023-06-18 01:27:01 +0200

    Add note to readline.py to remind of its use case
    The local package version of `readline.py` is less important now that
    there is an override mechanism. I kept it though because it still caters
    for cases like ActiveState Python which has no readline module at all.
    In those cases it is sufficient to install gnureadline and be done with
    it, with no need for the override.
    
    Add a note to explain this in case I forget again :-) And remind folks
    not to go down the dark `sys.path` with this module.

--- readline.py
+++ readline.py
@@ -1,2 +1,19 @@
+#
+# WARNING
+# -------
+#
+# This module is meant for distributions like ActiveState Python that have
+# no default readline module at all. Since there is no builtin readline that
+# clashes with this, it is straightforward to enable readline support:
+# simply install gnureadline and you are done.
+#
+# If your Python ships with readline, please don't see this module as an
+# encouragement to move site-packages higher up in `sys.path` or to perform
+# any other PYTHONPATH shenanigans in order to override the system readline.
+# That decision will come back to haunt you.
+#
+# Instead, run the override_readline script that only overrides readline and
+# nothing else, using site customization. Run `python -m override_readline`.
+#
 from gnureadline import *
 from gnureadline import __doc__
